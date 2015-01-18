fd582e9de6de7eec63102006574e69e2f4c00089
Walsh Costigan <walshcostigan@gmail.com>
2015-01-18 21:41:01 +0000

    added csv uploader

---
+++ app/models/company.rb
@@ -0,0 +1,9 @@
+require 'csv'
+
+class Company < ActiveRecord::Base
+    def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Company.create! row.to_hash
+    end
+  end
+end
