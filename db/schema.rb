55de550d49493d50cbece5d138c39c4d7d02dbdd
Walsh Costigan <walshcostigan@gmail.com>
2015-01-31 16:50:25 +0000

    added status type dropdown to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119215315) do
+ActiveRecord::Schema.define(version: 20150131164234) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
